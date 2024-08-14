be51dbfda2d517389980f52c706cbb8b3594554b
wolly2884 <117057806+wolly2884@users.noreply.github.com>
2024-08-14 16:54:12 -0300

    Update 00-laravel-deploy.sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
-echo "Running composer"
-composer global require hirak/prestissimo
+echo "--------------------->Running composer------------------------------------------"
 composer install 
 
-echo "Caching config..."
-php artisan config:cache
-
-echo "Caching routes..."
-php artisan route:cache
-
-echo "Running migrations..."
-php artisan migrate --force
